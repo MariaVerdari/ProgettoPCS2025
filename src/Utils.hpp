a7aa550101f617084b45b7dceb86b93cfe7f76d7
Alessio Giacomarra <alessio@MacBook-Air-di-Alessio.local>
2025-05-27 14:35:29 +0200

    Icosaedro

--- src/Utils.hpp
+++ src/Utils.hpp
@@ -8,6 +8,7 @@ void Normalizzazione(auto& arr);
 namespace PolyhedronLibrary {
 	bool Tetraedro(PolyhedronMesh& mesh);
 	bool Ottaedro(PolyhedronMesh& mesh);
+	bool Icosaedro(PolyhedronMesh& mesh);
 
 }
 
